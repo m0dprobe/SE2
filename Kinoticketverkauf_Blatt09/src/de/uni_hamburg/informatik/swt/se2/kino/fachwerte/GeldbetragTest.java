13d0beb6d4a66d865592fdd601240eaf29c0e513
Alexander Timmermann <3timmerm@informatik.uni-hamburg.de>
2014-07-07 17:12:43 +0200

    all done

--- Kinoticketverkauf_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
+++ Kinoticketverkauf_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
@@ -54,10 +54,15 @@ public class GeldbetragTest
     {
         Geldbetrag betrag1 = new Geldbetrag(1042);
         Geldbetrag betrag2 = new Geldbetrag(523);
+        Geldbetrag betrag3 = new Geldbetrag(-542);
         
         assertEquals(15, Geldbetrag.add(betrag1, betrag2).getEuro());
         assertEquals(65, Geldbetrag.add(betrag1, betrag2).getCent());
         assertEquals("15,65", Geldbetrag.add(betrag1, betrag2).toString());
+        
+        assertEquals(5, Geldbetrag.add(betrag1, betrag3).getEuro());
+        assertEquals(0, Geldbetrag.add(betrag1, betrag3).getCent());
+        assertEquals("5,00", Geldbetrag.add(betrag1, betrag3).toString());
         
     }
     
@@ -100,5 +105,23 @@ public class GeldbetragTest
         assertEquals(23, new Geldbetrag(betrag).getEuro());
         assertEquals(42, new Geldbetrag(betrag).getCent());
         assertEquals("23,42", new Geldbetrag(betrag).toString());
+    }
+    
+    @Test
+    public final void testCompare()
+    {
+        Geldbetrag betrag1 = new Geldbetrag(500);
+        Geldbetrag betrag2 = new Geldbetrag("7");
+        Geldbetrag betrag3 = new Geldbetrag(700);
+        
+        assertTrue(betrag1.compareTo(betrag2) < 0);
+        assertTrue(betrag2.compareTo(betrag1) > 0);
+        assertTrue(betrag2.compareTo(betrag3) == 0);
+    }
+    
+    public final void testValidate()
+    {
+        assertFalse(Geldbetrag.validate("7,0"));
+        assertTrue(Geldbetrag.validate("7,00"));
     }
 }
